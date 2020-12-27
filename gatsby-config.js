2cce5c626e1b899985571cbfc293f8a6d95782e6
belal-mazlom <b.mazlom@gmail.com>
2020-12-27 22:53:57 +0200

    Update gatsby settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -55,11 +55,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
+        name: `Zomato React`,
         short_name: `starter`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#50BEBF`,
+        theme_color: `#50BEBF`,
         display: `minimal-ui`,
         icon: `src/assets/images/app-icon.png`, // This path is relative to the root of the site.
       },
